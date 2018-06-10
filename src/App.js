50569d393b2c0532e7d5546d1fe599a7816b67e6
Kondrat1991 <kondratmychailo1991@gmail.com>
2018-06-10 11:40:24 +0300

    import CostList into APP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, {Component, Fragment} from 'react';
 import './App.css';
+import CostList from './components/CostList/CostList';
 
 class App extends Component {
     render() {
         return (
             <Fragment>
                 <h1>Test</h1>
+                <CostList/>
+
             </Fragment>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 import {Provider} from 'react-redux';
 import store from './store/store';
-import CostList from './components/CostList/CostList';
 
 
 ReactDOM.render(
     <Provider store={store}>
-        <CostList/>
+        <App />
+
     </Provider>
     , document.getElementById('root'));
 
 
 
 
-// ReactDOM.render(
-//     <Provider store={store}>
-//         <App />
-//
-//     </Provider>
-//     , document.getElementById('root'));
 
