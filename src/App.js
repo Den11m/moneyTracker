165aa3b57d95b37bf181e54fcb9ccef3bc36d6a1
Katrin <katringm03@gmail.com>
2018-06-11 18:35:31 +0300

    add component add-costs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, {Component, Fragment} from 'react';
 import './App.css';
+import AddNewCosts from './components/addNewCosts/addNewCosts';
 
 class App extends Component {
     render() {
         return (
             <Fragment>
                 <h1>Test</h1>
+                < AddNewCosts />
             </Fragment>
         );
     }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
 import {combineReducers} from 'redux';
 import test from './testReducers';
-
+import costs from './addNewCostsReducer'
 
 const reducers = combineReducers({
-    test,
+    test, costs
 });
 
 
