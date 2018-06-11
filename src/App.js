7ac7846e4f1cd3523752c1929c16582ff0ad32e4
SapunkovAA <AASapunkovv@gmail.com>
2018-06-11 12:37:07 +0300

    'addenter'

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import React, {Component, Fragment} from 'react';
 import './App.css';
-import Modale from './components/Modale/Modale';
 import EnterForm from './components/RegistrationEnterForm/EnterForm';
-import RegistrationForm from './components/RegistrationEnterForm/RegistrationForm'
 
 class App extends Component {
     render() {
         return (
             <Fragment>
-                <Modale/>
                 <EnterForm/>
-                <RegistrationForm/>
             </Fragment>
         );
     }
