7cfcbfee10341cd288a67798bd2f9578f8b92c3f
Denis Minin <den11minin@gmail.com>
2018-06-12 09:48:29 +0300

    add func wich load/save to localStorage

--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {Provider} from 'react-redux';
-import store from './store/store';
+import configureStore from './store/store';
+import {loadState, saveState} from './localStorage';
+
+const persistedState = loadState();
+const store = configureStore(persistedState);
+
+setInterval(()=>{
+    store.subscribe(() => {
+        saveState({
+            costs: store.getState().costs,
+            budget: store.getState().budget,
+            isLogin: store.getState().isLogin,
+            period: store.getState().period,
+        })
+    });
+},1000);
 
 ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>
     , document.getElementById('root'));
-
--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,12 @@
 import {createStore, applyMiddleware} from 'redux';
 import reducers from '../reducers/index';
 import thunk from 'redux-thunk';
-import {composeWithDevTools} from 'redux-devtools-extension';
+import { composeWithDevTools} from 'redux-devtools-extension';
 
-const store = createStore(
-    reducers,
-    composeWithDevTools(),
-    applyMiddleware(thunk),
-);
+const middleware = [thunk];
 
-export default store;
+const enchancer = composeWithDevTools(applyMiddleware(...middleware));
+
+export default function configureStore(persistedState = {}) {
+    return createStore(reducers, persistedState, enchancer);
+}
