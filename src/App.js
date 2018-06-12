ea8ea8c9911d4ae994404f92bad8a39ee68407b9
Nathan Drake <galchuksergii@gmail.com>
2018-06-12 15:27:53 +0300

    filter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component, Fragment} from 'react';
 import './App.css';
 import Modale from './components/Modale/Modale';
+import Filter from "./components/Filter/Filter";
 
 class App extends Component {
     render() {
         return (
             <Fragment>
                 <Modale/>
+                <Filter/>
             </Fragment>
         );
     }
---
+++ src/components/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Main = (props) => {
+    return (
+        <div>
+            <h3 className='main'>Вход</h3>
+            <input type="text"/>
+        </div>
+    );
+};
+
+Main.propTypes = {};
+Main.defaultProps = {};
+
+export default Main;
