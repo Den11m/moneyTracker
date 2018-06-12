bfac795f7b6d684a0c1ea588ddf0aaeade5fdaf7
Katrin <katringm03@gmail.com>
2018-06-12 14:09:56 +0300

    change component add-new-costs

--- src/components/addNewCosts/addNewCosts.js
+++ src/components/addNewCosts/addNewCosts.js
@@ -9,12 +9,10 @@ import './style.css';
 const AddNewCosts = (props) => {
   
 let sumInput = '',
-    // calendarInput = '',
     commentInput = '';
     let date = new Date();
     console.log(date);
     let test;
-    console.log(test)
 
 
 
@@ -24,73 +22,73 @@ let sumInput = '',
             <div className='icons-category'ref={(input)=> test = input}>
                 <div className='icon-category'>
                 <input type="radio" className='radio' id="health" name="contact" value="здоровье"/>
-                <label for='health'  className='health' data-category='здоровье'>
+                <label htmlFor='health'  className='health' data-category='здоровье'>
                 </label>
                 <p className='category--text'>здоровье</p>
                 </div>
                 <div className='icon-category'>
                 <input type="radio" className='radio' id="food" name="contact" value="еда"/>
-                <label for='food'  className='food' data-category='еда'>
+                <label htmlFor='food'  className='food' data-category='еда'>
                 </label>
                 <p className='category--text'>еда</p>
                 </div>
                 <div className='icon-category'>
                 <input type="radio" className='radio' id="hygiene" name="contact" value="гигиена"/>
-                <label for='hygiene'  className='hygiene' data-category='гигиена'>
+                <label htmlFor='hygiene'  className='hygiene' data-category='гигиена'>
                 </label>
                 <p className='category--text'>гигиена</p>
                 </div>
                 <div className='icon-category'>
                 <input type="radio" className='radio' id="home" name="contact" value="жилье"/>
-                <label for='home'  className='home' data-category='жилье'>
+                <label htmlFor='home'  className='home' data-category='жилье'>
                 </label>
                 <p className='category--text'>жилье</p>
                 </div>
                 <div className='icon-category'>
                 <input type="radio" className='radio' id="clothes" name="contact" value="одежда"/>
-                <label for='clothes'  className='clothes' data-category='одежда'>
+                <label htmlFor='clothes'  className='clothes' data-category='одежда'>
                 </label>
                 <p className='category--text'>одежда</p>
                 </div>
                 <div className='icon-category'>
                 <input type="radio" className='radio' id="sport" name="contact" value="спорт"/>
-                <label for='sport'  className='sport' data-category='спорт'>
+                <label htmlFor='sport'  className='sport' data-category='спорт'>
                 </label>
                 <p className='category--text'>спорт</p>
                 </div>
                 <div className='icon-category'>
                 <input type="radio" className='radio' id="relax" name="contact" value="отдых"/>
-                <label for='relax'  className='relax' data-category='отдых'>
+                <label htmlFor='relax'  className='relax' data-category='отдых'>
                 </label>
                 <p className='category--text'>отдых</p>
                 </div>
                 <div className='icon-category'>
                 <input type="radio" className='radio' id="communication" name="contact" value="связь"/>
-                <label for='communication'  className='communication' data-category='связь'>
+                <label htmlFor='communication'  className='communication' data-category='связь'>
                 </label>
                 <p className='category--text'>связь</p>
                 </div>
                 <div className='icon-category'>
                 <input type="radio" className='radio' id="transport" name="contact" value="транспорт"/>
-                <label for='transport'  className='transport' data-category='транспорт'>
+                <label htmlFor='transport'  className='transport' data-category='транспорт'>
                 </label>
                 <p className='category--text'>транспорт</p>
                 </div>
                 <div className='icon-category'>
                 <input type="radio" className='radio' id="nursling" name="contact" value="питомцы"/>
-                <label for='nursling'  className='nursling' data-category='питомцы'>
+                <label htmlFor='nursling'  className='nursling' data-category='питомцы'>
                 </label>
                 <p className='category--text'>питомцы</p>
                 </div>
                 <div className='icon-category'>
                 <input type="radio" className='radio' id="present" name="contact" value="подарки"/>
-                <label for='present'  className='present' data-category='подарки'>
+                <label htmlFor='present'  className='present' data-category='подарки'>
                 </label>
                 <p className='category--text'>подарки</p>
                 </div>
                 <div className='icon-category'>
                 <input type="radio" className='radio' id="other" name="contact" value="другое"/>
-                <label for='other'  className='other' data-category='другое'>
+                <label htmlFor='other'  className='other' data-category='другое'>
                 </label>
                 <p className='category--text'>другое</p>
                 </div>
@@ -107,7 +105,6 @@ let sumInput = '',
             // console.log('click',Array.from(test.children).find(el => el.children[0].checked === true).children[0].value)
             }}>coxpанить</button>
         </div> 
-
     )
 }
 function MDTP(dispatch){
