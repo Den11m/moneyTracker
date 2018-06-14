ac85ed7b7a2fbaab9404a25e136ea132bed9cf2c
Michael <mike-l@ukr.net>
2018-06-14 11:53:59 +0300

    add spendperday

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -1,30 +1,27 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {connect} from 'react-redux';
 import Modale from '../Modale/Modale';
-import { addBudget } from '../../actions/budgetAction';
-import PropTypes from 'prop-types';
+import {addBudget} from '../../actions/budgetAction';
 import moment from 'moment';
-import toggleShowWindow from '../../actions/clickAction';
-import {click} from '../../selectors/CostListSelector';
 import toggleShowBudget from '../../actions/budgetShowAction';
 import './budget.css';
 
 const Budget = (props) => {
-        let budgetInput = '';
-        let dateInput = '';
+    let budgetInput = '';
+    let dateInput = '';
 
     function checkPeriod(values) {
-        switch(values){
+        switch (values) {
             case 'day' :
                 return {
                     start: moment().startOf('day').valueOf(),
                     end: moment().endOf('day').valueOf(),
-                }
+                };
             case 'week' :
                 return {
-                    start: moment().startOf('week').add(1, 'days').valueOf(),
-                    end: moment().endOf('week').add(1, 'days').valueOf(),
-                }
+                    start: moment().startOf('week').valueOf(),
+                    end: moment().endOf('week').valueOf(),
+                };
             case 'month' :
                 return {
                     start: moment().startOf('month').valueOf(),
@@ -33,30 +30,39 @@ const Budget = (props) => {
         }
     }
 
-        return (
-            <Modale
-                // toggleShowWindow={props.toggleShowWindow} click={props.click}>
-                toggleShowWindow={props.toggleShowBudget} click={props.onClickBudget} >
-                <h2 className='budget-title'>Создать бюджет</h2>
+    return (
+        <Modale
+            toggleShowWindow={props.toggleShowBudget} click={props.onClickBudget}>
+            <h2 className='budget-title'>Создать бюджет</h2>
 
-                <form className='budget-form' action='#' onSubmit={(e)=> props.getSum(e, {plan: +budgetInput.value, fact:0, date:checkPeriod(dateInput.value)})}>
+            <form className='budget-form' action='#' onSubmit={(e) => props.getSum(e, {
+                plan: +budgetInput.value,
+                fact: 0,
+                date: checkPeriod(dateInput.value),
+                spendPerDay: +budgetInput.value / Math.ceil(moment.duration(checkPeriod(dateInput.value).end - checkPeriod(dateInput.value).start).asDays()),
+            })}>
 
-                    <input className='input-budget' type="number" placeholder='Сумма' ref={(input) => budgetInput=input} />
+                <input className='input-budget statistic__select' type="number" placeholder='Сумма' ref={(input) => budgetInput = input}/>
 
-                    <select className='budget-select' ref={(input)=> dateInput=input}>
-                        <option value="month">Месяц</option>
-                        <option value="week">Неделя</option>
-                        <option value="day">День</option>
-                    </select>
-                    <button className='modale__btn-save'
-                            onClick={(e)=>{
-                                props.getSum(e, {plan: +budgetInput.value, fact:0, date:checkPeriod(dateInput.value)});
-                                props.toggleShowBudget()}}
-                    >СОХРАНИТЬ
-                    </button>
-                </form>
-            </Modale>
-        )
+                <select className='budget-select statistic__select' ref={(input) => dateInput = input}>
+                    <option value="month">Месяц</option>
+                    <option value="week">Неделя</option>
+                    <option value="day">День</option>
+                </select>
+                <button className='modale__btn-save'
+                        onClick={(e) => {
+                            props.getSum(e, {
+                                plan: +budgetInput.value,
+                                fact: 0,
+                                date: checkPeriod(dateInput.value),
+                                spendPerDay: +budgetInput.value / Math.ceil(moment.duration(checkPeriod(dateInput.value).end - checkPeriod(dateInput.value).start).asDays()),
+                            });
+                            props.toggleShowBudget();}}
+                >СОХРАНИТЬ
+                </button>
+            </form>
+        </Modale>
+    )
 };
 
 function MDTP(dispatch) {
@@ -69,21 +75,14 @@ function MDTP(dispatch) {
         toggleShowBudget: function () {
             dispatch(toggleShowBudget())
         },
-
-        // toggleShowWindow: function () {
-        //     dispatch(toggleShowWindow())
-        // }
     }
 }
 
-function MSTP(state){
+function MSTP(state) {
     return {
-        // click: click(state),
         onClickBudget: state.budgetShow,
     }
-
 }
-
 
 
 export default connect(MSTP, MDTP)(Budget);
