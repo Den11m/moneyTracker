8c104493e6cbfe4ec6c403b9cd699eaafd243dcc
Denis Minin <den11minin@gmail.com>
2018-07-02 12:17:23 +0300

    adds user token

--- server/api/middlewares/auth.js
+++ server/api/middlewares/auth.js
@@ -2,11 +2,15 @@ const jwt = require('jsonwebtoken');
 const {secret} = require('../config/index');
 
 module.exports = (req, res, next) => {
-    try{
-       const token =  req.headers.authorization;
-       req.auth = jwt.verify(token, secret)
-
-
+    try {
+        const token = req.headers.authorization;
+        req.auth = jwt.verify(token, secret);
+        req.auth && console.log('Auth is successfull');
     }
-    catch(err){}
+    catch (err) {
+        console.log(err);
+        res.status(401).json({
+            message: 'token invalid'
+        })
+    }
 };
--- server/app.js
+++ server/app.js
@@ -3,12 +3,15 @@ const app = express();
 const path = require('path');
 const loginRouter = require('./api/routes/login');
 const signUpRouter = require('./api/routes/signup');
+const checkAuth = require('./api/middlewares/auth')
 
 app.use(express.json());
 app.use(express.urlencoded());
 
+
 app.use('/login',loginRouter);
 app.use('/signup', signUpRouter);
+app.use('/costs', checkAuth, ()=>{console.log('You are welcome!')});
 
 app.get('/', function (req, res, next) {
     res.sendFile(path.resolve(__dirname + '/../public/index.html'));
