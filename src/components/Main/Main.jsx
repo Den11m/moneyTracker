8b9e47dcf9a55861c9e042519e9583348f26ba67
Denis Minin <den11minin@gmail.com>
2018-07-06 13:35:14 +0300

    styled header

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Main.css';
-import CostList from '../../components/CostList/CostList';
+import CostList from '../CostList/CostList';
 import Budget from '../Budget/Budget';
 import SideBar from '../../components/sidebar/sidebar';
 import Statistics from '../../components/statistics/statistics';
