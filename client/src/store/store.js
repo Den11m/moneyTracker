3d774bb39376f03a4896092860ef00de4aa62d6f
Denis Minin <den11minin@gmail.com>
2018-07-09 21:21:22 +0300

    add redux-devtools-extension in package.json

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,7 +1,7 @@
 import {createStore, applyMiddleware} from 'redux';
 import reducers from '../reducers/index';
 import thunk from 'redux-thunk';
-import {composeWithDevTools} from 'redux-devtools-extension / developmentOnly';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import {createBrowserHistory} from 'history';
 import {connectRouter} from 'connected-react-router';
 
