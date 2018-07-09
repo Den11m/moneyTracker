14a879e0c56d322e5384f38b9a1ba96cc41fbaa9
Denis Minin <den11minin@gmail.com>
2018-07-09 17:00:18 +0300

    adds build

--- server/app.js
+++ server/app.js
@@ -28,7 +28,7 @@ app.use('/signup', signUpRouter);
 app.use('/costs', checkAuth, costsRouter);
 
 app.get('/', function (req, res, next) {
-    res.sendFile(path.resolve(__dirname + '/../public/index.html'));
+    res.sendFile(path.resolve(__dirname + '/../build/index.html'));
     // res.send("Work!");
 });
 
