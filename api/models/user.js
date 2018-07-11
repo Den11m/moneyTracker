ea59ddbedd90fd995409967d5fd88ff718f6045a
Vlad <gribennikov.vlad@gmail.com>
2018-07-11 18:05:36 +0300

    Архитектура бекенда

--- api/models/user.js
+++ api/models/user.js
@@ -2,12 +2,16 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 let budgetSchema = require('./budget');
 
-const checkEmail = /.+@{1}.\.{1}.+/gi;
+const checkEmail = /.+@{1}.+\.{1}.+/gi;
 
 let userSchema = new Schema({
-    email: {type: String, require:  true, unique: true, match: checkEmail},
-    password: {type: String, require: true, unique: true},
+    _id: mongoose.Schema.Types.ObjectId,
+    email: {type: String, required: true, unique: true, match: checkEmail},
+    password: {type: String, required: true, unique: true},
     budgets: [budgetSchema]
+}, {
+    toObject: { virtuals: true},
+    toJSON: { virtuals: true}
 });
 
 module.exports = mongoose.model('User', userSchema); /// verify "User" ?
--- api/routes/budget.js
+++ api/routes/budget.js
@@ -1,63 +1,60 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
-const Budget = require('../models/budget');
+const Users = require('../models/user');
 const jwt = require('jsonwebtoken');
 
 router.post('/', (req, res, next) => {
-     const decoded = jwt.decode(req.headers.authorization);
-     console.log('decoded', decoded);
-     const newBudget = new Budget({
-            _id: new mongoose.Types.ObjectId(),
-            userId: decoded._id,
-            plan: req.body.plan,
-            fact: req.body.fact,
-            date: req.body.date,
-            spendPerDay: req.body.spendPerDay,
-          })
-          .save()
-          .then(budget => {
-               res.status(201).json({
-                    Message: "Your budget saved",
-                    budget: budget,
-               })
-
-          })
-          .catch(error => {
-               res.status(500).json(error) //ошибки сервера статус 500 и выше
-          })
+    const decoded = jwt.decode(req.headers.authorization);
+    console.log('decoded', decoded);
+    Users.findOne({
+        _id: decoded._id
+    }).exec()
+        .then((user) => {
+            user.budgets.push(req.body);
+            return user.save();
+        })
+        .then((user) => {
+            const lastIndex = user.budgets.length - 1;
+            res.status(201).json({
+                Message: "Your budget saved",
+                budget: user.budgets[lastIndex],
+            })
+        })
+        .catch(error => {
+            res.status(500).json(error) //ошибки сервера статус 500 и выше
+        })
 });
 router.get('/', (req, res, next) => {
-     const decoded = jwt.decode(req.headers.authorization);
-
-     Budget.find({
-               userId: decoded._id
-          })
-          .exec()
-          .then(collect => {
-               res.status(200).json({
-                    Message: 'Your collection budgets',
-                    budgets: collect
-               })
-          })
-          .catch(error => {
-               res.status(500).json(error)
-          })
+    const decoded = jwt.decode(req.headers.authorization);
+    Users.findOne({
+        _id: decoded._id
+    })
+        .exec()
+        .then(user => {
+            res.status(200).json({
+                Message: 'Your collection budgets',
+                budgets: user.budgets
+            })
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
 
 });
 
 router.delete('/:id', (req, res, next) => {
     console.log('req params', req.params);
     Budget.findByIdAndRemove(req.params.id)
-          .exec()
-          .then(budget => {
-               res.status(200).json({
-                    Message: 'Your budget deleted ',
-                    budgetId: budget._id,
-               })
-          })
-          .catch(error => {
-               res.status(500).json(error)
-          })
+        .exec()
+        .then(budget => {
+            res.status(200).json({
+                Message: 'Your budget deleted ',
+                budgetId: budget._id,
+            })
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        })
 });
 module.exports = router;
